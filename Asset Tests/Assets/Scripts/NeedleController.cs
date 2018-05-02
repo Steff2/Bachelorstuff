6780c61db4e2ebf990a04ea6d51012b45df313a5
Stefan Müller <stmepp@online.de>
2018-05-02 20:51:10 +0200

    Added Seeds and cleaned up unnecessary scripts (RedDot)

---
+++ Asset Tests/Assets/Scripts/NeedleController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeedleController : MonoBehaviour {
+    //public int NeedleCount; //Amount of Needles placed
+    //public int MarkerCount; //Amount of Markers on the skin
+    public float speed; //Adjustement of the forward/backward movement of the needle
+
+    public GameObject Tumor; //The tumor Object
+    public Projector Marker; //The Object Projecting the Marker
+    public GameObject Seed;
+    public GameObject Skin; //The Skin Object
+
+    private Rigidbody rb;
+    [SerializeField]
+    //Range of position variation of the Marker
+    private Vector3 Range; //Adjustment of the Placement of the Markers
+
+    // Sets all the Markers on the Skin and places the Needles via Raycast
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        RaycastHit hit;
+
+        if (Physics.Raycast(Marker.transform.position, Marker.transform.forward, out hit))
+        {
+            var EntryDirection = Tumor.transform.position - hit.point;
+            gameObject.transform.position = hit.point - EntryDirection.normalized * 10;
+            gameObject.transform.LookAt(Tumor.transform);
+        }
+        /*
+        for (int i = 0; i < MarkerCount; i++)
+        {
+            var MarkerInst = Instantiate(Marker, Marker.transform.position);
+            MarkerInst.transform.position += new Vector3(Random.Range(15, 20), Random.Range(15, 20), 0);
+            if (Physics.Raycast(MarkerInst.transform.position, MarkerInst.transform.forward, out hit))
+            {
+                var EntryDirection = Tumor.transform.position - hit.point;
+                Instantiate(gameObject, transform.position);
+                gameObject.transform.position = hit.point - EntryDirection.normalized * 10;
+                gameObject.transform.LookAt(Tumor.transform);
+            }
+        }*/
+
+        //int x = 1;
+        //while(x!=NeedleCount)
+        //{
+        //  var AnotherNeedle = Instantiate(gameObject);
+        //}
+
+    }
+
+    // Update is called once per frame
+    // Movement of the Needle, vertical and horizontal
+    void Update () {
+        if (Input.GetKey("left"))
+        {
+            transform.Rotate(0, 0.25f, 0);
+        }
+
+        if (Input.GetKey("up"))
+        {
+            transform.Rotate(0.25f, 0, 0);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(0, -0.25f, 0);
+        }
+
+        if (Input.GetKey("down"))
+        {
+            transform.Rotate(-0.25f, 0, 0);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            rb.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            rb.MovePosition(transform.position - transform.forward * speed * Time.deltaTime);
+        }
+
+        if(Input.GetKeyUp("space"))
+        {
+            var InstSeed = Instantiate(Seed);
+            InstSeed.transform.position = transform.position;
+        }
+    }
+}
--- Asset Tests/Assets/Scripts/USCameraController.cs
+++ Asset Tests/Assets/Scripts/USCameraController.cs
@@ -7,21 +7,26 @@ public class USCameraController : MonoBehaviour {
     public Transform needle;
     public Transform tumor;
     public Transform FixPoint;
-    public Transform RedDot;
-	
-	// Move and place the camera
-	void Update () {
-        var DotToTumor = tumor.position - RedDot.position;
-        //var NeedleToTumor = tumor.position - needle.position;
-        Vector3 NeedleToFixpoint;
-        var NeedleToX = needle.forward - needle.position;
-        //Get the relative position by creating a plane with the vector from needle to tumor and a random other vector
-        FixPoint.position = tumor.position + 40 * DotToTumor.normalized;
-        NeedleToFixpoint = FixPoint.position - needle.position;
-        var PerpVect = Vector3.Cross(NeedleToFixpoint, NeedleToX);
-        PerpVect.Normalize();
-        gameObject.transform.position = FixPoint.position + 100 * PerpVect;
+    public GameObject Marker;
 
-        gameObject.transform.LookAt(FixPoint);
+    RaycastHit hit;
+    Vector3 NeedleToFixpoint;
+
+    // Move and place the camera
+    void Update () {
+        if (Physics.Raycast(Marker.transform.position, Marker.transform.forward, out hit))
+        {
+            var DotToTumor = tumor.position - hit.point;
+            var NeedleToMark = needle.forward - needle.position;
+
+            //Get the relative position by creating a plane with the vector from needle to tumor and a random other vector
+            FixPoint.position = tumor.position + 40 * DotToTumor.normalized;
+            NeedleToFixpoint = FixPoint.position - needle.position;
+            var PerpVect = Vector3.Cross(NeedleToFixpoint, NeedleToMark);
+            PerpVect.Normalize();
+            gameObject.transform.position = FixPoint.position + 100 * PerpVect;
+
+            gameObject.transform.LookAt(FixPoint);
+        }
 	}
 }
