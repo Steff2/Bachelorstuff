b5278cf6ae32ef647bfd71fbb42cabccdae30186
Stefan <s.mueller@stud.uni-heidelberg.de>
2018-05-11 15:17:54 +0200

    Added Start screen to the game and to SRS

---
+++ Asset Tests/Assets/TextHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TextHandler : MonoBehaviour {
+    public Text CameraHelper;
+    public Image USCamera;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Text>().enabled = true;
+        CameraHelper.enabled = false;
+        USCamera.enabled = false;
+
+	}
+	
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            gameObject.GetComponent<Text>().enabled = false;
+            CameraHelper.enabled = true;
+            USCamera.enabled = true;
+        }
+    }
+}
