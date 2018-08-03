d5598b84f466e6dea4541c7502c4887db04b320e
Stefan Müller <stmepp@online.de>
2018-08-03 09:28:00 +0200

    fixed Entryposition and cleaned up

--- Asset Tests/Assets/Scripts/EntrypointPosition.cs
+++ Asset Tests/Assets/Scripts/EntrypointPosition.cs
@@ -1,13 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EntrypointPosition : MonoBehaviour {
 
     public GameObject entryPoint1;
     public GameObject entryPoint2;
-    public GameObject entryPoint3;
-    public GameObject entryPoint4;
+    //public GameObject entryPoint3;
+    //public GameObject entryPoint4;
     public GameObject entryPoint5;
 
     Vector3 first_point_vector;
@@ -25,22 +23,23 @@ public class EntrypointPosition : MonoBehaviour {
     // Use this for initialization
     void Start () {
 
+        GameObject.Find("ScriptManager").GetComponent<Needle_Movement>().enabled = false;
         position_counter = 1;
         position_locked = false;
 
         first_point_vector = entryPoint1.transform.position - tumorPoint.transform.position;
         second_point_vector = entryPoint2.transform.position - tumorPoint.transform.position;
-        third_point_vector = entryPoint3.transform.position - tumorPoint.transform.position;
-        fourth_point_vector = entryPoint4.transform.position - tumorPoint.transform.position;
+        //third_point_vector = entryPoint3.transform.position - tumorPoint.transform.position;
+        //fourth_point_vector = entryPoint4.transform.position - tumorPoint.transform.position;
         fifth_point_vector = entryPoint5.transform.position - tumorPoint.transform.position;
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        while (position_locked == false)
-        {
-            if(Input.GetKey(KeyCode.LeftArrow))
+        //while (position_locked == false)
+        //{
+            if(Input.GetKeyDown(KeyCode.LeftArrow))
             {
                 position_counter--;
 
@@ -48,9 +47,42 @@ public class EntrypointPosition : MonoBehaviour {
                 {
                     position_counter = 5;
                 }
+
+                if (position_counter == 1)
+                {
+                    gameObject.transform.position = entryPoint1.transform.position - 60 * first_point_vector.normalized;
+                    gameObject.transform.LookAt(tumorPoint.transform.position);
+                    gameObject.transform.Rotate(-90, 0, 0);
+                }
+
+                if (position_counter == 2)
+                {
+                    gameObject.transform.position = entryPoint2.transform.position - 60 * second_point_vector.normalized;
+                    gameObject.transform.LookAt(tumorPoint.transform.position);
+                    gameObject.transform.Rotate(-90, 0, 0);
+                }
+
+                /*if (position_counter == 3)
+                {
+                    gameObject.transform.position = entryPoint3.transform.position + 30 * third_point_vector.normalized;
+                    gameObject.transform.LookAt(tumorPoint.transform.position);
+                }*/
+
+                /*if (position_counter == 4)
+                {
+                    gameObject.transform.position = entryPoint4.transform.position + 30 * fourth_point_vector.normalized;
+                    gameObject.transform.LookAt(tumorPoint.transform.position);
+                }*/
+
+                if (position_counter == 5)
+                {
+                    gameObject.transform.position = entryPoint5.transform.position - 60 * fifth_point_vector.normalized;
+                    gameObject.transform.LookAt(tumorPoint.transform.position);
+                    gameObject.transform.Rotate(-90, 0, 0);
+                }
             }
 
-            if (Input.GetKey(KeyCode.RightArrow))
+            if (Input.GetKeyDown(KeyCode.RightArrow))
             {
                 position_counter++;
             
@@ -58,43 +90,47 @@ public class EntrypointPosition : MonoBehaviour {
                 {
                     position_counter = 1;
                 }
-            }
 
-            if(position_counter == 1)
-            {
-                gameObject.transform.position = entryPoint1.transform.position + 30 * first_point_vector.normalized;
-                gameObject.transform.LookAt(tumorPoint.transform.position);
-            }
+                if (position_counter == 1)
+                {
+                    gameObject.transform.position = entryPoint1.transform.position - 60 * first_point_vector.normalized;
+                    gameObject.transform.LookAt(tumorPoint.transform.position);
+                    gameObject.transform.Rotate(-90, 0, 0);
+                }
 
-            if (position_counter == 2)
-            {
-                gameObject.transform.position = entryPoint2.transform.position + 30 * second_point_vector.normalized;
-                gameObject.transform.LookAt(tumorPoint.transform.position);
-            }
+                if (position_counter == 2)
+                {
+                    gameObject.transform.position = entryPoint2.transform.position - 60 * second_point_vector.normalized;
+                    gameObject.transform.LookAt(tumorPoint.transform.position);
+                    gameObject.transform.Rotate(-90, 0, 0);
+                }
 
-            if (position_counter == 3)
-            {
-                gameObject.transform.position = entryPoint3.transform.position + 30 * third_point_vector.normalized;
-                gameObject.transform.LookAt(tumorPoint.transform.position);
-            }
+                /*if (position_counter == 3)
+                {
+                    gameObject.transform.position = entryPoint3.transform.position + 30 * third_point_vector.normalized;
+                    gameObject.transform.LookAt(tumorPoint.transform.position);
+                }*/
 
-            if (position_counter == 4)
-            {
-                gameObject.transform.position = entryPoint4.transform.position + 30 * fourth_point_vector.normalized;
-                gameObject.transform.LookAt(tumorPoint.transform.position);
-            }
+                /*if (position_counter == 4)
+                {
+                    gameObject.transform.position = entryPoint4.transform.position + 30 * fourth_point_vector.normalized;
+                    gameObject.transform.LookAt(tumorPoint.transform.position);
+                }*/
 
             if (position_counter == 5)
-            {
-                gameObject.transform.position = entryPoint5.transform.position + 30 * fifth_point_vector.normalized;
-                gameObject.transform.LookAt(tumorPoint.transform.position);
+                {
+                    gameObject.transform.position = entryPoint5.transform.position - 60 * fifth_point_vector.normalized;
+                    gameObject.transform.LookAt(tumorPoint.transform.position);
+                    gameObject.transform.Rotate(-90, 0, 0);
+                }
             }
 
-            if(Input.GetKey(KeyCode.Space))
+            if(Input.GetKeyUp(KeyCode.Space))
             {
                 position_locked = true;
+                GameObject.Find("ScriptManager").GetComponent<Needle_Movement>().enabled = true;
             }
 
-        }
+        //}
     }
 }
