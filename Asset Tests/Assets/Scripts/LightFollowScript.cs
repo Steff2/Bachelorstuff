4065c7795d9c693903e644a65ed39a54a5482b18
Stefan Müller <stmepp@online.de>
2018-08-27 18:11:13 +0200

    Added Surgeon lights with script
    Added new Mouse movement modes

---
+++ Asset Tests/Assets/Scripts/LightFollowScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFollowScript : MonoBehaviour {
+
+    public Transform toFollow;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = toFollow.position - transform.position;
+    }
+    void Update()
+    {
+        transform.position = toFollow.position - offset;
+        transform.LookAt(toFollow);
+    }
+}
---
+++ Asset Tests/Assets/Scripts/Mouse_Needle_Movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse_Needle_Movement : MonoBehaviour {
+
+    private float speed = 10.0f;
+    int Mode = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Mode == 0)
+        {
+            transform.Translate(Input.GetAxis("Mouse X") * Time.deltaTime * speed, 0, Input.GetAxis("Mouse Y") * Time.deltaTime * speed, Space.Self);
+        }
+
+        if (Mode == 1)
+        {
+            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"),0, -Input.GetAxis("Mouse X")) * Time.deltaTime * speed);
+        }
+
+        if (Mode == 2)
+        {
+            transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * Time.deltaTime * speed * 10, 0));
+        }
+
+        if (Mode == 3)
+        {
+            transform.Translate(0, 0, Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * speed, Space.Self);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            Mode = Mode + 1;
+
+            if (Mode == 4)
+            {
+                Mode = 0;
+            }
+        }
+    }
+}
