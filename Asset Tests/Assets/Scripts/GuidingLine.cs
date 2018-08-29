f042bd85060dff93cfa1b8e30da84e4a102ca759
Stefan Müller <stmepp@online.de>
2018-08-29 20:36:37 +0200

    Added plane with Line now instead of only line component

---
+++ Asset Tests/Assets/Scripts/GuidingLine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuidingLine : MonoBehaviour {
+
+    public Vector3 startingPoint;
+    public Vector3 endPoint;
+
+    // Use this for initialization
+    void Start () {
+
+        LineRenderer line = gameObject.GetComponent<LineRenderer>();
+
+        line.sortingLayerName = "OnTop";
+        line.sortingOrder = 5;
+        line.positionCount = 2;
+        line.SetPosition(0, startingPoint);
+        line.SetPosition(1, endPoint);
+        line.startWidth = 0.5f;
+        line.endWidth = 0.5f;
+        line.useWorldSpace = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
