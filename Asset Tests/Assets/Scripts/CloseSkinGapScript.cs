654936bb24c230cf25bf5b0a552307fab34b6084
Stefan Müller <stmepp@online.de>
2018-09-10 23:46:40 +0200

    Changed Meshsplit and added CloseSkinGap

---
+++ Asset Tests/Assets/Scripts/CloseSkinGapScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseSkinGapScript : MonoBehaviour {
+
+    public GameObject needle;
+    public GameObject Cylinder;
+
+
+    RaycastHit hit;
+    Vector3 RayDirection;
+
+    // Use this for initialization
+    void Start () {
+        RayDirection = transform.TransformDirection(-Vector3.up);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Physics.Raycast(Cylinder.transform.position, RayDirection, out hit))
+        {
+            if((Cylinder.transform.position - hit.point).magnitude > 70)
+            {
+                needle.transform.Translate(0, -1f, 0);
+            }
+        }
+	}
+}
