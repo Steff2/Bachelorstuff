d8b7ca20d05bd41abf650530579df93dec3908e6
Stefan Müller <stmepp@online.de>
2018-10-07 16:53:23 +0200

    Added ScoreText to visualize the score at the end of the surgery Added if-statements for writing function in HandleScoreTextFile to differentiate the cases for dangerradii trigger in ScoreText visual Made last score and new score appear in ScoreText to directly compare (still if-statements there in read functions, maybe changing)

--- Asset Tests/Assets/Scripts/HandleScoreTextFile.cs
+++ Asset Tests/Assets/Scripts/HandleScoreTextFile.cs
@@ -1,28 +1,58 @@
 ﻿using UnityEngine;
-using UnityEditor;
-using System.IO;
+using UnityEngine.UI;
 using System;
+using System.IO;
 
 public class HandleScoreTextFile : MonoBehaviour {
 
-    public static void WriteString()
+    public Text ScoreText;
+    public GameObject ScoreTextObject;
+
+    public static void WriteString(Text ScoreText, GameObject ScoreTextObject)
     {
+        ScoreTextObject.GetComponent<Text>().enabled = true;
         string path = "Assets/Scores.txt";
 
-        //Write some text to the test.txt file
+        //Writes text to a score file
         StreamWriter writer = new StreamWriter(path, true);
         writer.WriteLine("take " + FeedbackStorage.callCounter);
-        writer.WriteLine("danger_Radius " + FeedbackStorage.DangerRadiusTriggered);
         writer.WriteLine("outer_Radius " + FeedbackStorage.LessDangerousRadiusTriggered);
+        writer.WriteLine("danger_Radius " + FeedbackStorage.DangerRadiusTriggered);
+
+        //Writes the scores into the Text object while writing into file
+        if(FeedbackStorage.DangerRadiusTriggered)
+        {
+            ScoreText.text = "You came too close to risky structures" + "\n";
+        }
+
+        if (!FeedbackStorage.DangerRadiusTriggered && FeedbackStorage.LessDangerousRadiusTriggered)
+        {
+            ScoreText.text = "You managed to avoid danger radius but still got too close" + "\n";
+        }
+
+        if (!FeedbackStorage.DangerRadiusTriggered && !FeedbackStorage.LessDangerousRadiusTriggered)
+        {
+            ScoreText.text = "You managed to avoid any risky structures" + "\n";
+        }
+
         writer.WriteLine("duration_to_Entrypoint " + FeedbackStorage.DurationToEntry);
+
+        ScoreText.text += "Your time until the needle entered the skin: " + FeedbackStorage.DurationToEntry + "\n";
+
         writer.WriteLine("duration_after_Entrypoint " + FeedbackStorage.DurationAfterEntry);
+
+        ScoreText.text += "Your time until the seed was placed: " + FeedbackStorage.DurationAfterEntry + "\n";
+
         writer.WriteLine("distance_to_Surface " + FeedbackStorage.DistanceToSurface);
-        writer.WriteLine(" ");
+
+        ScoreText.text += "The Distance from Surface to end position is: " + FeedbackStorage.DistanceToSurface + "\n";
+
+        writer.WriteLine("\n");
         writer.Close();
 
     }
 
-    static void ReadString()
+    static void ReadString(Text ScoreText)
     {
         string path = "Assets/Scores.txt";
 
@@ -33,10 +63,11 @@ public class HandleScoreTextFile : MonoBehaviour {
         int SkipLinesCounter = 0;
 
         string line;
-        string text;
-
+        //Reads the lines out of the score files, every 8th line is a new set of value for a new round played
         while ((line = reader.ReadLine()) != null)
         {
+            //since this compares the current entry values with the ones from the round before
+            //skip while there is less than two rounds played (14 lines)
             if (SkipLinesCounter > (lineCount - 2 * 7))
             {
                 continue;
@@ -44,36 +75,60 @@ public class HandleScoreTextFile : MonoBehaviour {
 
             else
             {
-                text = reader.ReadLine();
-                string[] splitString = text.Split(new string[] { " " }, StringSplitOptions.None);
+                line = reader.ReadLine();
+
+                //split the name/value pair
+                string[] splitString = line.Split(new string[] { " " }, StringSplitOptions.None);
 
                 if (splitString[0] == "outer_radius")
                 {
-                    if(FeedbackStorage.LessDangerousRadiusTriggered == bool.Parse(splitString[1]))
+                    if(FeedbackStorage.LessDangerousRadiusTriggered == true && FeedbackStorage.LessDangerousRadiusTriggered == bool.Parse(splitString[1]))
                     {
-
+                        ScoreText.text += "Again too close to risky structures (30)" + "\n";
                     }
-                    else
-                    {
 
+                    if (FeedbackStorage.LessDangerousRadiusTriggered == false && FeedbackStorage.LessDangerousRadiusTriggered == bool.Parse(splitString[1]))
+                    {
+                        ScoreText.text += "Managed to avoid risky structures again (30)" + "\n";
+                    }
+
+                    if (FeedbackStorage.LessDangerousRadiusTriggered == true && !FeedbackStorage.LessDangerousRadiusTriggered == bool.Parse(splitString[1]))
+                    {
+                        ScoreText.text += "Too close to risky structures this time (30)" + "\n";
+                    }
+
+                    if (FeedbackStorage.LessDangerousRadiusTriggered == false && !FeedbackStorage.LessDangerousRadiusTriggered == bool.Parse(splitString[1]))
+                    {
+                        ScoreText.text += "Managed to avoid risky structures (30)" + "\n";
                     }
                 }
                 if (splitString[0] == "danger_Radius")
                 {
-                    if(FeedbackStorage.DangerRadiusTriggered == bool.Parse(splitString[1]))
+                    if (FeedbackStorage.DangerRadiusTriggered == true && FeedbackStorage.LessDangerousRadiusTriggered == bool.Parse(splitString[1]))
                     {
-
+                        ScoreText.text += "Again way too close to risky structures (15)" + "\n";
                     }
-                    else
-                    {
 
+                    if (FeedbackStorage.DangerRadiusTriggered == false && FeedbackStorage.LessDangerousRadiusTriggered == bool.Parse(splitString[1]))
+                    {
+                        ScoreText.text += "Managed to avoid very risky structures again (15)" + "\n";
+                    }
+
+                    if (FeedbackStorage.DangerRadiusTriggered == true && !FeedbackStorage.LessDangerousRadiusTriggered == bool.Parse(splitString[1]))
+                    {
+                        ScoreText.text += "Way too close to risky structures this time (15)" + "\n";
+                    }
+
+                    if (FeedbackStorage.DangerRadiusTriggered == false && !FeedbackStorage.LessDangerousRadiusTriggered == bool.Parse(splitString[1]))
+                    {
+                        ScoreText.text += "Managed to avoid very risky structures (15)" + "\n";
                     }
                 }
                 if (splitString[0] == "duration_to_Entrypoint")
                 {
                     if(FeedbackStorage.DurationToEntry < float.Parse(splitString[1]))
                     {
-
+                        ScoreText.text += "In your last attempt your time before entering was: " + FeedbackStorage.DurationToEntry;
                     }
                     if(FeedbackStorage.DurationToEntry > float.Parse(splitString[1]))
                     {
@@ -88,7 +143,7 @@ public class HandleScoreTextFile : MonoBehaviour {
                 {
                     if (FeedbackStorage.DurationAfterEntry < float.Parse(splitString[1]))
                     {
-
+                        ScoreText.text += "In your last attempt your time after entering was: " + FeedbackStorage.DurationAfterEntry;
                     }
                     if (FeedbackStorage.DurationAfterEntry > float.Parse(splitString[1]))
                     {
@@ -103,7 +158,7 @@ public class HandleScoreTextFile : MonoBehaviour {
                 {
                     if (FeedbackStorage.DistanceToSurface < float.Parse(splitString[1]))
                     {
-
+                        ScoreText.text += "In your last attempt your needles distance to the surface was: " + FeedbackStorage.DistanceToSurface;
                     }
                     if (FeedbackStorage.DistanceToSurface > float.Parse(splitString[1]))
                     {
@@ -116,16 +171,21 @@ public class HandleScoreTextFile : MonoBehaviour {
                 }
                 SkipLinesCounter++;
             }
-            reader.Close();
         }
+        reader.Close();
+    }
+
+    void Start()
+    {
+        ScoreTextObject.GetComponent<Text>().enabled = false;
     }
 
     void Update()
     {
         if (Input.GetKeyUp("b"))
         {
-            WriteString();
-            ReadString();
+            WriteString(ScoreText, ScoreTextObject);
+            ReadString(ScoreText);
         }
     }
 
--- Asset Tests/Assets/Scripts/TextHandler.cs
+++ Asset Tests/Assets/Scripts/TextHandler.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 /// <summary>
