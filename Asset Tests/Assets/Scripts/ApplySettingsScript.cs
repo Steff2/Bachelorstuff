139508da448785e5f55e193f0b736fecf9dc395d
Stefan Müller <stmepp@online.de>
2018-10-12 08:05:59 +0200

    added text toggling for ApplySettingsScript

--- Asset Tests/Assets/Scripts/ApplySettingsScript.cs
+++ Asset Tests/Assets/Scripts/ApplySettingsScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 //Apply the chosen guidance settings from the menu scene on the scripts
@@ -8,16 +9,22 @@ public class ApplySettingsScript : MonoBehaviour {
 
     EntrypointPosition Guidance_Script;
     Mouse_Needle_Movement No_Guidance_Script;
+    Text TextForGuidance;
+    Text TextWithoutGuidance;
 
     // Use this for initialization
     void Start () {
         Guidance_Script = GameObject.Find("NeedleTip").GetComponent<EntrypointPosition>();
         No_Guidance_Script = GameObject.Find("Cylinder").GetComponent<Mouse_Needle_Movement>();
+        TextForGuidance = GameObject.Find("PlanText").GetComponent<Text>();
+        TextWithoutGuidance = GameObject.Find("StartingScreen").GetComponent<Text>();
 
-        if(FeedbackStorage.guiding_Assistance)
+        if (FeedbackStorage.guiding_Assistance)
         {
             Guidance_Script.enabled = true;
             No_Guidance_Script.enabled = false;
+            TextForGuidance.enabled = true;
+            TextWithoutGuidance.enabled = true;
             Debug.Log("test guidance script");
             Debug.Log(Guidance_Script.enabled);
             Debug.Log("test no guidance script");
@@ -27,6 +34,8 @@ public class ApplySettingsScript : MonoBehaviour {
         {
             Guidance_Script.enabled = false;
             No_Guidance_Script.enabled = true;
+            TextForGuidance.enabled = false;
+            TextWithoutGuidance.enabled = false;
             Debug.Log("test guidance script");
             Debug.Log(Guidance_Script.enabled);
             Debug.Log("test no guidance script");
